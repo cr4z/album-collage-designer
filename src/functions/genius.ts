24deadd23d75cbea5929c8e7377860b79c8dcc8f
steven <stevencr7zz@gmail.com>
2020-11-24 15:17:08 -0600

    Finish download system

--- src/functions/genius.ts
+++ src/functions/genius.ts
@@ -1,30 +1,21 @@
 export const getImagesFromInput = async (input: string, callback: Function) => {
   const encodedInput = encodeURIComponent(input);
 
+  //you can add autocomplete!
   const response = await fetch(
-    `https://api.genius.com/search?q=${encodedInput}&per_page=50`,
+    `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${encodedInput}`,
     {
       method: "GET",
-      headers: {
-        "x-rapidapi-key": "df21c1b749mshac0f907e51d8e2cp13ef4djsna9639bf0f0d3",
-        "x-rapidapi-host": "genius.p.rapidapi.com",
-      },
     }
   );
 
   const json = await response.json();
 
-  const hits = json.response.hits;
+  const hits = json.data;
 
   const sources: string[] = [];
-
   hits.forEach((hit: any) => {
-    const albumSrc = hit.result.header_image_url;
-    if (!sources.includes(albumSrc)) sources.push(albumSrc);
-  });
-
-  hits.forEach((hit: any) => {
-    const songSrc = hit.result.song_art_image_url;
+    const songSrc = hit.album.cover_xl;
     if (!sources.includes(songSrc)) sources.push(songSrc);
   });
 
