cb26deac68ac332946d175621812baac9d7ad256
steven <stevencr7zz@gmail.com>
2020-12-03 15:05:29 -0600

    Add backend

--- server.js
+++ server.js
@@ -1,62 +1,55 @@
 const express = require("express");
-// eslint-disable-next-line no-unused-vars
-// const bodyParser = require('body-parser');
+const generateCanvas = require("./logic/generateCanvas");
+const fetch = require("node-fetch");
 const path = require("path");
+
+//?
+require("dotenv").config();
+
 const app = express();
-const port = process.env.PORT || 8080;
+const port = 3001;
 
-app.use(express.static(path.join(__dirname, "build")));
+//?
+app.use(express.json());
 
-// This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "build", "index.html"))
+);
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.get("/log/:msg", (req, res) => {
+  console.log(req.params.msg);
+});
 
-//const generateCanvas = require("./logic/generateCanvas");
-//const fetch = require("node-fetch");
-// //?
-// require("dotenv").config();
+app.get("/generateCanvas/:columns/:rows/:encodedJson", async (req, res) => {
+  const columns = req.params.columns;
+  const rows = req.params.rows;
 
-// const app = express();
-// const port = 3001;
+  const json = decodeURIComponent(req.params.encodedJson);
+  const imageSources = JSON.parse(json);
 
-// //?
-// app.use(express.json());
+  const canvas = await generateCanvas(imageSources, {
+    numColumns: columns,
+    numRows: rows,
+  });
 
-// app.get("/log/:msg", (req, res) => {
-//   console.log(req.params.msg);
-// });
+  const url = canvas.toDataURL();
 
-// app.get("/generateCanvas/:columns/:rows/:encodedJson", async (req, res) => {
-//   const columns = req.params.columns;
-//   const rows = req.params.rows;
+  res.json(url); //write streams response
+  console.log("served url!");
+});
 
-//   const json = decodeURIComponent(req.params.encodedJson);
-//   const imageSources = JSON.parse(json);
+app.get("/proxy/:request", async (req, res) => {
+  const encodedRequest = req.params.request;
+  const unencryptedRequest = decodeURIComponent(encodedRequest);
+  const result = await fetch(unencryptedRequest, {
+    method: "GET",
+  });
 
-//   const canvas = await generateCanvas(imageSources, {
-//     numColumns: columns,
-//     numRows: rows,
-//   });
+  const json = await result.json();
 
-//   const url = canvas.toDataURL();
+  res.json(json);
+});
 
-//   res.json(url); //write streams response
-//   console.log("served url!");
-// });
-
-// app.get("/proxy/:request", async (req, res) => {
-//   const encodedRequest = req.params.request;
-//   const unencryptedRequest = decodeURIComponent(encodedRequest);
-//   const result = await fetch(unencryptedRequest, {
-//     method: "GET",
-//   });
-
-//   const json = await result.json();
-
-//   res.json(json);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
