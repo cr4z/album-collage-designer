280e8690ff4edfa71fd369477ba1265178c3c6ef
steven <stevencr7zz@gmail.com>
2020-12-03 17:41:01 -0600

    Add favicon

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "build")));
 
