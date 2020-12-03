09f57137ac40fbf93352301dfaac7dd0a39b2c64
steven <stevencr7zz@gmail.com>
2020-12-03 14:04:23 -0600

    Attempt

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 //const generateCanvas = require("./logic/generateCanvas");
 //const fetch = require("node-fetch");
