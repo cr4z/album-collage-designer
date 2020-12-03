e0bc57ee23dfdd578b34b8d0bb1ed9861d12eebf
steven <stevencr7zz@gmail.com>
2020-12-03 15:33:07 -0600

    Remove unnecessary code

--- server.js
+++ server.js
@@ -5,20 +5,11 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 5000;
 
-//?
-app.use(express.json());
-require("dotenv").config();
-
 app.use(express.static(path.join(__dirname, "build")));
 
-// This route serves the React app
 app.get("/", (req, res) =>
   res.sendFile(path.resolve(__dirname, "build", "index.html"))
 );
-
-app.get("/log/:msg", (req, res) => {
-  console.log(req.params.msg);
-});
 
 app.get("/generateCanvas/:columns/:rows/:encodedJson", async (req, res) => {
   const columns = req.params.columns;
